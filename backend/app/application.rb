7f225d1dde035568f2a874b4a68af6447138060e
Brosner522 <rosnerbb@gmail.com>
2021-06-02 12:06:31 -0400

    log in

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -18,6 +18,26 @@ class Application
 
     return [200, { 'Content-Type' => 'application/json' }, [ topping ]]
 
+  elsif req.path.match(/customer/) && req.get? 
+    customer = Customer.all.to_json
+    return [200, { 'Content-Type' => 'application/json' }, [ customer ]]
+
+  elsif req.path.match(/customer/) && req.post? 
+    customer_hash = JSON.parse(req.body.read)
+
+ 
+    filter_customer = Customer.any? do |customer|
+      customer_hash == customer
+    end
+
+    if filter_customer
+    new_customer = Customer.create(customer_hash)
+
+    return [201, { 'Content-Type' => 'application/json' }, [ new_customer.display_data ]]
+    else
+    return [400, {}]
+    end
+
     else
       resp.write "Path Not Found"
     end
